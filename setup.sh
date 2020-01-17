bdd1cdf826a08e1253420bd7c9859bf63db3995a
Mukesh Yadav <tmp@itzmukeshy7.dev>
2020-01-17 13:48:22 +0530

    Setup changes

--- setup.sh
+++ setup.sh
@@ -14,10 +14,10 @@ set -o nounset    # fail on unset variables
 TICKS=$(echo $(date +%s | cut -b1-13))
 
 # Name of your team (optional)
-HEROKU_TEAM_NAME="appcloud-dev"
+HEROKU_TEAM_NAME=""
 
 # Descriptive name for the Heroku app
-HEROKU_APP_NAME="MyLightningApp"
+HEROKU_APP_NAME="itzsfdx"
 
 # Name of the Heroku apps you'll use
 HEROKU_DEV_APP_NAME="dev$TICKS"
@@ -30,14 +30,14 @@ HEROKU_PIPELINE_NAME="pipeline$TICKS"
 # Usernames or aliases of the orgs you're using
 DEV_HUB_USERNAME="HubOrg"
 DEV_USERNAME="DevOrg"
-STAGING_USERNAME="TestOrg"
-PROD_USERNAME="ProdOrg"
+STAGING_USERNAME="DevOrg"
+PROD_USERNAME="DevOrg"
 
 # Repository with your code
-GITHUB_REPO="wadewegner/salesforce-dx-pipeline-sample"
+GITHUB_REPO="itzmukeshy7/salesforce-dx-pipeline-sample"
 
 # Your package name
-PACKAGE_NAME="PipelineSamplePackage2"
+PACKAGE_NAME="SamplePackage"
 
 ### Setup script
 
